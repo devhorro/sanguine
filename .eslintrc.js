d978ec2693763c88684833cc5a6723b7d18279c2
devhorro <ethansnewhcim@gmail.com>
2022-07-10 14:18:36 +0100

    eslint and prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
@@ -13,7 +14,11 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
+    "linebreak-style": 0,
     semi: ["error", "always"],
     quotes: ["error", "double"],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   },
 };
